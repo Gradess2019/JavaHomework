b3941a771fde15c663bf7e01ed5ca1d09dabb1f9
Stepan <trofimovjava@gmail.com>
2019-09-05 21:45:49 +0300

    Add Lab1

---
+++ Labs/Lab1/src/com/gradess/company/Main.java
@@ -0,0 +1,10 @@
+package com.gradess.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String argument : args) {
+            System.out.println(argument);
+        }
+    }
+}
